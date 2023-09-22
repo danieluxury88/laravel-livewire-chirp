dc0295021a81e1f9b3004eea50450d585850c0c7
Daniel Proaño Chacon <danieluxury@gmail.com>
2023-09-21 22:52:57 -0500

    Add Chirps on navigation template

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+    <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -14,6 +14,9 @@
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('chirps')" :active="request()->routeIs('chirps')">
+                        {{ __('Chirps') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -69,6 +72,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('chirps')" :active="request()->routeIs('chirps')">
+                {{ __('Chirps') }}
             </x-responsive-nav-link>
         </div>
 
